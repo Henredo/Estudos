88f999f5c260de48c4d49c368a5d3fde53a123e9
Henry M. Leme <henrymleme1000@gmail.com>
2026-03-18 11:22:47 -0300

    salvo novo brquets

--- ED/braquets.c
+++ ED/braquets.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct stack
 {
@@ -10,8 +11,62 @@ void iniciar_pilha(stack *input){
     stack* novo = malloc(sizeof(stack));
     input = novo;
 }
+int push(stack *stack,int input){
+    if (stack->huggers_n >= 20){
+        printf("\nError : overflow");
+        return 1;
+    }
+    else{
+        stack->huggers[stack->huggers_n] = input;
+        stack->huggers_n++;
+        return 0;
+    }
+       
+}
+int pop(stack *stack){
+    if (stack->huggers_n <= 0){
+        printf("\nError : underflow");
+        return 1;
+    }
+    else{
+        stack->huggers_n--;
+        stack->huggers[stack->huggers_n] = 0;
+    }
+       
+}
 
 void add_huggers(stack *input){
+    for (int i = 0; i < strlen(input->string); i++){
+        switch (input->string[i])
+        {
+        case '(':
+            push(input,1);
+        break;
+        
+        case ')':
+            pop(input);
+            break;
+        case '[':
+            push(input,2);
+            break;
+        
+        case ']':
+            pop(input);
+            break;
+        
+        
+        case '{':
+            push(input,3);
+            break;
+        
+        case '}':
+            pop(input);
+            break;
+        
+        default:
+            break;
+        }
+    }
     
 }
 
