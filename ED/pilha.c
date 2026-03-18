b6a0193b66650d0a33669013b88f418cf75cab1d
Henry M. Leme <henrymleme1000@gmail.com>
2026-03-18 10:09:19 -0300

    pilha rudimentar feita

--- ED/pilha.c
+++ ED/pilha.c
@@ -8,15 +8,42 @@ typedef struct pilha{
 
 } pilha;
 
+void iniciar_pilha(pilha *stack){
+    pilha* novo = malloc(sizeof(pilha));
+    stack = novo;
+}
+
 int read_stack(pilha *stack){
-    for (int i = 0; i < MAX; i++){
+    for (int i = 0; i < stack->quantidade; i++){
         printf("\n%d",stack->stack[i]);
     }
     return 0;
 }
 
 int push(pilha *stack,int input){
-    
+    if (stack->quantidade >= MAX){
+        printf("\nError : overflow");
+        return 1;
+    }
+    else{
+        stack->stack[stack->quantidade] = input;
+        stack->quantidade++;
+        return 0;
+    }
+       
+}
+
+int pop(pilha *stack){
+    if (stack->quantidade <= 0){
+        printf("\nError : underflow");
+        return 1;
+    }
+    else{
+        stack->quantidade--;
+        stack->stack[stack->quantidade] = 0;
+        //free(&stack->stack[stack->quantidade-1]);
+    }
+       
 }
 
 void main(){
